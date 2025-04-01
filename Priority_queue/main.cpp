a3d223267235848a03e52965e65f3d3b20b7898e
Даниил Ликутин <likutindan@gmail.com>
2025-04-01 16:28:20 +0300

    all done

---
+++ Priority_queue/main.cpp
@@ -0,0 +1,152 @@
+#include <queue>
+#include <deque>
+#include "myQueue.h"
+#include <random>
+#include <chrono>
+#include <iostream>
+#include <time.h>
+#include <algorithm>
+#include <fstream>
+
+using namespace std::chrono;
+
+int main()
+{
+	int progon = 10;
+	std::cout << "TIME MESURE: " << std::endl;
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::vector<long long> nums;
+	std::vector<long long> nums2;
+	std::uniform_int_distribution<> dis2(1000, 1000000);
+	for (int i = 0; i < 10000; i++)
+	{
+		nums.push_back(dis2(gen));
+	}
+	for (int i = 0; i < 100; i++)
+	{
+		nums2.push_back(dis2(gen));
+	}
+	{
+		long long duration = 0;
+		for (int i = 0; i < progon; i++)
+		{
+			
+			high_resolution_clock::time_point t1 = high_resolution_clock::now();
+			myQueue<long long,std::vector<long long>> q(nums);
+			high_resolution_clock::time_point t2 = high_resolution_clock::now();
+			duration += std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+		}
+		std::cout << "myQueue create time: " << duration / progon << std::endl;
+	}
+	{
+		long long duration = 0;
+		for (int i = 0; i < progon; i++)
+		{
+			high_resolution_clock::time_point t1 = high_resolution_clock::now();
+			
+			for (int j = 0; j < nums.size(); j++)
+			{
+				std::priority_queue<long long, std::vector<long long>,std::less<long long>> q(nums.begin(),nums.end());
+			}
+			high_resolution_clock::time_point t2 = high_resolution_clock::now();
+			duration += std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+		}
+		std::cout << "std create time: " << duration / progon << std::endl;
+	}
+	{
+		long long duration = 0;
+		for (int i = 0; i < progon; i++)
+		{
+			myQueue<long long, std::vector<long long>> q(nums);
+			high_resolution_clock::time_point t1 = high_resolution_clock::now();
+			for (int j = 0; j < nums.size()/4; j++)
+			{
+				q.top();
+				q.pop();
+
+			}
+			high_resolution_clock::time_point t2 = high_resolution_clock::now();
+			duration += std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+		}
+		std::cout << "myQueue top time: " << duration / progon << std::endl;
+	}
+	{
+		long long duration = 0;
+		for (int i = 0; i < progon; i++)
+		{
+			std::priority_queue<long long, std::vector<long long>, std::less<long long>> q(nums.begin(), nums.end());
+			high_resolution_clock::time_point t1 = high_resolution_clock::now();
+			for (int j = 0; j < nums.size()/4; j++)
+			{
+				q.top();
+				q.pop();
+			}
+			high_resolution_clock::time_point t2 = high_resolution_clock::now();
+			duration += std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+		}
+		std::cout << "std top time: " << duration / progon << std::endl;
+	}
+	{
+		long long duration = 0;
+		for (int i = 0; i < progon; i++)
+		{
+			myQueue<long long, std::vector<long long>> q(nums);
+			high_resolution_clock::time_point t1 = high_resolution_clock::now();
+			for (int j = 0; j < nums2.size(); j++)
+			{
+				q.insert(nums2[j]);
+
+			}
+			high_resolution_clock::time_point t2 = high_resolution_clock::now();
+			duration += std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+		}
+		std::cout << "myQueue insert 100 time: " << duration / progon << std::endl;
+	}
+	{
+		long long duration = 0;
+		for (int i = 0; i < progon; i++)
+		{
+			std::priority_queue<long long, std::vector<long long>, std::less<long long>> q(nums.begin(), nums.end());
+			high_resolution_clock::time_point t1 = high_resolution_clock::now();
+			for (int j = 0; j < nums2.size(); j++)
+			{
+				q.push(nums2[i]);
+			}
+			high_resolution_clock::time_point t2 = high_resolution_clock::now();
+			duration += std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+		}
+		std::cout << "std insert 100 time: " << duration / progon << std::endl;
+	}
+	std::cout << "---------------" << std::endl;
+	myBinQueue<std::string> my;
+	std::priority_queue<std::string> standart;
+	std::string word;
+	{
+		long long duration = 0;
+		std::fstream file;
+		file.open("big.txt");
+		high_resolution_clock::time_point t1 = high_resolution_clock::now();
+		while (file >> word)
+		{
+			standart.push(word);
+		}
+		high_resolution_clock::time_point t2 = high_resolution_clock::now();
+		duration += std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+		std::cout << "myBin big.txt time and word: " << duration << " " << standart.top() << std::endl;
+	}
+	{
+		long long duration = 0;
+		std::fstream file;
+		file.open("big.txt");
+		high_resolution_clock::time_point t1 = high_resolution_clock::now();
+		while (file >> word)
+		{
+			my.insert(word);
+		}
+		high_resolution_clock::time_point t2 = high_resolution_clock::now();
+		duration += std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+		std::cout << "myBin big.txt time and word: " << duration << " " << my.top() << std::endl;
+	}
+	
+}
---
+++ Priority_queue/myQueue.h
@@ -0,0 +1,170 @@
+#pragma once
+#include <vector>
+#include <functional>
+#include <algorithm>
+#include <iostream>
+
+template <
+    typename T,
+    typename Cont = std::vector<T>,
+    typename Comp = std::less<T>
+>
+class myQueue
+{
+    Comp myComp;
+    Cont myCont;
+public:
+    myQueue(Cont c = Cont(), Comp cmp = Comp()) :myComp(cmp), myCont(c)
+    {
+        std::sort(myCont.begin(), myCont.end(), myComp);
+    }
+
+    void insert(T elem)
+    {
+        myCont.insert(std::upper_bound(myCont.begin(), myCont.end(), elem, myComp),elem);
+    }
+
+    T top()
+    {
+        return myCont.back();
+    }
+
+    Cont container()
+    {
+        return myCont;
+    }
+
+    void pop()
+    {
+        myCont.pop_back();
+    }
+
+    myQueue concat(const myQueue& other)
+    {
+        Cont shared(myCont);
+        shared.insert(shared.end(), other.begin(), other.end());
+        //std::sort(shared.begin(), shared.end(), myComp);
+        return myQueue(shared);
+    }
+
+    template <
+        typename T,
+        typename Cont = std::vector<T>,
+        typename Comp = std::less<T>
+    >
+    friend std::ostream& operator<<(std::ostream& os, const myQueue& q)
+    {
+        for (const auto& x : q.container())
+        {
+            os << x << " ";
+        }
+        os << "\n";
+        return os;
+    }
+};
+
+template <
+    typename T,
+    typename Cont = std::vector<T>,
+    typename Comp = std::less<T>
+>
+class myBinQueue
+{
+    Comp myComp;
+    Cont myCont;
+public:
+    myBinQueue(Cont c = Cont(), Comp cmp = Comp()) :myComp(cmp), myCont(c)
+    {
+        for (size_t i = myCont.size(); i >= 0; i--)
+        {
+            sift_down(i);
+        }
+    }
+
+    void insert(T elem)
+    {
+        myCont.push_back(elem);
+        sift_up(myCont.size());
+    }
+
+    T top()
+    {
+        return myCont.front();
+    }
+
+    Cont container()
+    {
+        return myCont;
+    }
+
+    void pop()
+    {
+        if (myCont.empty()) return;
+        myCont[0] = myCont.back();
+        myCont.pop_back();
+        if (!myCont.empty()) {
+            sift_down(0);
+        }
+    }
+
+    myBinQueue concat(const myBinQueue& other)
+    {
+        Cont shared(myCont);
+        shared.insert(shared.end(), other.begin(), other.end());
+        //std::sort(shared.begin(), shared.end(), myComp);
+        return myBinQueue(shared);
+    }
+
+    template <
+        typename T,
+        typename Cont = std::vector<T>,
+        typename Comp = std::less<T>
+    >
+    friend std::ostream& operator<<(std::ostream& os, const myBinQueue& q)
+    {
+        for (const auto& x : q.container())
+        {
+            os << x << " ";
+        }
+        os << "\n";
+        return os;
+    }
+private:
+    size_t left(size_t ind)
+    {
+        return 2 * ind + 1;
+    }
+    size_t right(size_t ind)
+    {
+        return 2 * ind + 2;
+    }
+    size_t parent(size_t ind)
+    {
+        return (ind - 1) / 2;
+    }
+    void sift_down(size_t ind)
+    {
+        auto largest = ind;
+        if (left(ind) < myCont.size() && myComp(myCont[largest], myCont[left(ind)]))
+        {
+            largest = left(ind);
+        }
+        if (right(ind) < myCont.size() && myComp(myCont[largest], myCont[right(ind)]))
+        {
+            largest = right(ind);
+        }
+        if (largest != ind)
+        {
+            std::swap(myCont[ind], myCont[largest]);
+            sift_down(largest);
+        }
+    }
+    void sift_up(size_t ind)
+    {
+        while (ind > 0 && myComp(myCont[parent(ind)], myCont[ind]))
+        {
+            std::swap(myCont[parent(ind)], myCont[ind]);
+            ind = parent(ind);
+        }
+    }
+};
